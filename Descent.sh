c990f4df24bfb91b022be9f41e69f3263de0c53e
JeodC <47716344+JeodC@users.noreply.github.com>
2023-11-18 20:58:29 -0500

    Fix libs dir

--- Descent.sh
+++ Descent.sh
@@ -21,7 +21,7 @@ $ESUDO rm -rf ~/.$GAME
 ln -sfv $GAMEDIR/conf/.$GAME ~/
 
 export LIBGL_FB=4
-export LD_LIBRARY_PATH=$GAMEDIR/lib:/usr/lib
+export LD_LIBRARY_PATH=$GAMEDIR/libs:/usr/libs
 
 $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 666 /dev/uinput
